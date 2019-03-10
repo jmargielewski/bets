43db9a3d3df4ea8243ea2264cca9327e5939ce1a
Kuba Margielewski <kuba@mbp-kuba.home>
2019-03-10 14:18:05 +0000

    remove duplicated code in endpoints

--- api/server.js
+++ api/server.js
@@ -5,15 +5,6 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
 
-// "proxy": {
-//   "/decimalOddsLessThanTwo": {
-//     "target": "http://localhost:4000"
-//   },
-//   "/decimalOddsMoreThanTwo": {
-//     "target": "http://localhost:4000"
-//   }
-// },
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
@@ -25,46 +16,22 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(cors());
+
 const reqPath = path.join(__dirname, "./data/data.json");
 const bets = JSON.parse(fs.readFileSync(reqPath, "utf8")).bets;
 
-// function getDecimalOdds(operator) {
-//   const b = bets.map(bet => bet.odds.some(odd => odd.oddsDecimal>2) && bet).filter(Boolean);
-//   const r = bets.reduce((acc, bet) => {
-//     console.log('acc', acc);
-//     console.log('bet', bet);
-//     const k = bet.odds.filter(odd =>
-//       odd.oddsDecimal > 2
-//      )
-//      console.log(' yo oy',k);
-//     return k && acc.push(bet);
-//   }, []);
-
-//     if(operator) {
-//       return r
-//     }
-
-// }
-// getDecimalOdds(true);
-//  console.log('heloo',getDecimalOdds(true));
-
-app.use(cors());
+const getDecimal = cb =>
+  bets
+    .map(bet => bet.odds.some(odd => cb(odd.oddsDecimal, 2)) && bet)
+    .filter(Boolean);
 
 app.get("/decimalOddsMoreThanTwo", (req, res) => {
-  // code here
-  const s = bets
-    .map(bet => bet.odds.some(odd => odd.oddsDecimal > 2) && bet)
-    .filter(Boolean);
-  res.send(s);
+  res.send(getDecimal((v, limit) => v > limit));
 });
 
 app.get("/decimalOddsLessThanTwo", (req, res) => {
-  // code here
-  const m = bets
-    .map(bet => bet.odds.some(odd => odd.oddsDecimal < 2) && bet)
-    .filter(Boolean);
-
-  res.send(m);
+  res.send(getDecimal((v, limit) => v < limit));
 });
 
 app.listen(4000, () => {
